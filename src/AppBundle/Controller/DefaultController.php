14ccadabd8ece905495d3886a7e1596e4c267b64
Andy <coding@andysmiles4games.com>
2016-03-25 19:11:07 +0100

    Add base layout and login screen
    I have decided to use Bootstrap as my CSS framework. It includes
    several components which will help to create a nice looking UI
    without taking any effort in terms of working on style and layout.
    I have added an empty stylesheet file for further changes in terms
    of style in the future.
    
    Also added the very first files of my login page.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * BlogSystem - Login as the default controller.
+ *
+ * @version 	0.1.0 22-Mar-16
+ * @copyright   Copyright (c) 2016 by Andy Liebke. All rights reserved. (http://andysmiles4games.com)
+ */
 class DefaultController extends Controller
 {
     /**
@@ -13,9 +18,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('login/index.html.twig', [
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..')
+        ]);
     }
 }
